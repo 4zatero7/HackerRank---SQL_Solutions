6e3981871b63a70b16e9a07e69af52a3797b14dd
Azat Erol <80638377+4zatero7@users.noreply.github.com>
2021-04-01 17:54:23 +0200

    Create Weather Observation Station 13.sql
    Solution

---
+++ Aggregation/Weather Observation Station 13.sql
@@ -0,0 +1,8 @@
+/************************
+@Author: Azat Erol
+Always happy coding!
+************************/
+
+SELECT ROUND(SUM(LAT_N),4)
+FROM STATION
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
